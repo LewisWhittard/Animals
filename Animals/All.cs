9db674bc862ae749798a67e6d2b8ecbcddb9a371
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-11-15 14:20:51 +0000

    Page All

---
+++ Animals/All.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isolated
+{
+    public enum AnimalType
+    {
+        Fish,
+        Amphibian,
+        Reptile,
+        Mammal,
+        Bird,
+        invertebrate,
+    }
+
+    public interface IAnimal
+    {
+        public AnimalType AnimalType { get; set; }
+    }
+
+    internal abstract class AnimalBase : IAnimal
+    {
+        protected AnimalType _animalType;
+
+
+
+
+        AnimalType IAnimal.AnimalType
+        {
+            get { return _animalType; }
+            set { _animalType = value; }
+        }
+
+    }
+
+    internal abstract class AnimalBasePlus : AnimalBase
+    {
+
+
+        AnimalType GetAnimalType() { return ((IAnimal)this).AnimalType; }
+        // Method to set animal type
+        public void SetAnimalType(AnimalType animalType)
+        {
+            ((IAnimal)this).AnimalType = animalType;
+        }
+    }
+}
